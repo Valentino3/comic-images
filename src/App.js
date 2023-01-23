70da6c790c1995f431788b28c5d1dffd740ecb33
Valentino3 <valentino.rota3@gmail.com>
2023-01-23 16:32:06 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import images from './images/images.json'
+
+function getMeta(url, callback) {
+  const img = new Image();
+  img.src = url;
+  img.onload = function() { callback(this.width, this.height); }
+}
+
+function pageCounter(amount){
+
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Testing Json Image Imports
+      <br></br>
+      {images &&
+        images.map((image) => {
+          console.log(image.id)
+          getMeta(image.content, (width, height) => {console.log(width,height) });
+          return (
+            <div className="box" key={image.id}>
+              <div> {image.title} </div>
+
+              <img
+                id={image.id}
+                style={{ width: "900px", height: "100%" }}
+                src={image.content}
+              />
+            </div>
+          );
+        })}
     </div>
   );
 }
